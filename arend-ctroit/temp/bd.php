edaa48e8768dfeefff14f775eba5e1b7994da7d1
liesenko990-ux <liesenko990@gmail.com>
2026-09-17 12:59:30 +0300

    Delete arend-ctroit directory

--- arend-ctroit/temp/bd.php
+++
@@ -1,4 +0,0 @@
-<?php
-$mysqli=new mysqli("localhost","root","","arend-stroit");
-$mysqli->set_charset("utf8");
-?>
--- arend-ctroit/temp/footer.php
+++
@@ -1,4 +0,0 @@
-</div>
-<footer class="bg-warning p-4 mb-3"></footer>
-</body>
-</html>
